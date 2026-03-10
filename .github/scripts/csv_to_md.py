0f8ed0bd92ad5da3856ca9f191f60eaa5972a7a7
Oliver Rietmann <oliver.rietmann@cern.ch>
2026-03-10 14:23:58 +0100

    Use emojis instead of html

--- .github/scripts/csv_to_md.py
+++ .github/scripts/csv_to_md.py
@@ -5,17 +5,7 @@ import tabulate as tab
 csv_benchmark = sys.argv[1]
 csv_baseline = sys.argv[2]
 
-def html_inline(x):
-  if x == 0:
-    color = "black"
-    x = "{:.1f}".format(x)
-  elif x < 0:
-    color = "green"
-    x = "{:.1f}".format(x)
-  elif x > 0:
-    color = "red"
-    x = "+{:.1f}".format(x)
-  return f"<span style=\"color:{color}\">{x}</span>"
+pretty = lambda x : "{:.1f}".format(x) if x <= 0 else "+{:.1f}".format(x)
 
 with open(csv_benchmark) as csv_file:
   csv_reader = csv.reader(csv_file)
@@ -33,11 +23,12 @@ for benchmark, baseline in zip(table_benchmark, table_baseline):
   name = benchmark[0]
   time = benchmark[1]
   d = float(baseline[1]) - float(benchmark[1])
-  difference = html_inline(d)
-  percent = html_inline(100 * d / float(baseline[1]))
+  emoji = ':red_circle:' if 0 < d else ':green_circle:'
+  difference = pretty(d)
+  percent = pretty(100 * d / float(baseline[1]))
   count = benchmark[2]
   events = benchmark[3]
-  table.append([name, time, difference, percent, count, events])
+  table.append([name, time, emoji, difference, percent, count, events])
 
-header = ["name", "time", "difference", "percent", "count", "events"]
+header = ["name", "time", "", "difference", "percent", "count", "events"]
 print(tab.tabulate(table, header, tablefmt="github"))
