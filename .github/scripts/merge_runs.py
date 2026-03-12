d02b151e0dcd24066c49de09a14b105df1287b16
Oliver Rietmann <oliver.rietmann@cern.ch>
2026-03-12 14:15:16 +0100

    Merge 40 runs and compute stdev

--- .github/scripts/merge_runs.py
+++ .github/scripts/merge_runs.py
@@ -1,13 +1,15 @@
-import sys
+import argparse
 import csv
 import statistics
 
-in_csv = sys.argv[1]
-out_csv = sys.argv[2]
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--discard', type=int, default=0, help='Number of initial measurements to discard')
+parser.add_argument('-i', '--input', required=True, help='Input CSV file')
+parser.add_argument('-o', '--output', required=True, help='Output CSV file')
+args = parser.parse_args()
 
 time_dict = dict({})
-
-with open(in_csv) as csv_file:
+with open(args.input) as csv_file:
   csv_reader = csv.reader(csv_file)
   next(csv_reader)
   for name, time, _, _ in csv_reader:
@@ -18,10 +20,10 @@ with open(in_csv) as csv_file:
 
 data = [["name", "time", "stdev"]]
 for name, time_list in time_dict.items():
-  mean = statistics.mean(time_list)
-  stdev = statistics.stdev(time_list)
+  mean = statistics.mean(time_list[args.discard:])
+  stdev = statistics.stdev(time_list[args.discard:])
   data.append([name, mean, stdev])
 
-with open(out_csv, 'w') as csv_file:
+with open(args.output, 'w') as csv_file:
   csv_writer = csv.writer(csv_file)
   csv_writer.writerows(data)
