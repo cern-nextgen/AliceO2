47b07d57334b4141b5d773cf4b10b93ebe755224
Oliver Rietmann <oliver.rietmann@cern.ch>
2026-04-09 15:47:45 +0200

    Add total time of baseline

--- .github/scripts/csv_to_md.py
+++ .github/scripts/csv_to_md.py
@@ -38,12 +38,13 @@ for baseline, current in zip(table_baseline, table_current):
   baseline_name, baseline_mean, baseline_stdev, count_baseline = baseline
   name, mean, stdev, count = current
   assert(baseline_name == name)
+  total_time_baseline = baseline_mean * (count_baseline // args.runs)
   total_time = mean * (count // args.runs)
   d, s, t = student(baseline_mean, baseline_stdev, args.runs, mean, stdev, args.runs)
   emoji = get_emoji(t)
-  table.append([name, int(total_time), int(mean), f'{stdev:.2f}', count, int(baseline_mean), f'{baseline_stdev:.2f}', count_baseline, f'{d:.2f}', f'{s:.2f}', f'{t:.2f}', emoji])
+  table.append([name, int(total_time), int(mean), f'{stdev:.2f}', count, int(total_time_baseline), int(baseline_mean), f'{baseline_stdev:.2f}', count_baseline, f'{d:.2f}', f'{s:.2f}', f'{t:.2f}', emoji])
 
 table.sort(key = lambda row: row[1], reverse=True)
 
-header = ['name', 'total time (\u03BCs)', 'mean (\u03BCs)', 'stdev \u03C3', 'samples', 'mean (old)', 'stdev (old)', 'samples (old)', 'diff \u0394', 'stdev (pooled)', 't', '']
+header = ['name', 'total time (\u03BCs)', 'mean (\u03BCs)', 'stdev \u03C3', 'samples', 'total time (base)', 'mean (base)', 'stdev (base)', 'samples (base)', 'diff \u0394', 'stdev (pooled)', 't', '']
 print(tab.tabulate(table, header, tablefmt="github"))
