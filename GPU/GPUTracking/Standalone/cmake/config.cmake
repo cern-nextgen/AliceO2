f9c6b912b117ba1ab56114b1406e5e8e3d224604
Oliver Rietmann <oliver.rietmann@cern.ch>
2026-04-20 17:51:58 +0200

    Make #include <meta> work

--- GPU/GPUTracking/Standalone/cmake/config.cmake
+++ GPU/GPUTracking/Standalone/cmake/config.cmake
@@ -17,7 +17,7 @@ set(ENABLE_HIP 0)
 set(ENABLE_OPENCL 0)
 set(GPUCA_CONFIG_VC 1)
 set(GPUCA_CONFIG_FMT 1)
-set(GPUCA_CONFIG_ROOT 1)
+set(GPUCA_CONFIG_ROOT 0)
 set(GPUCA_CONFIG_ONNX 0)
 set(GPUCA_BUILD_EVENT_DISPLAY 0)
 set(GPUCA_BUILD_EVENT_DISPLAY_FREETYPE 1)
@@ -26,7 +26,7 @@ set(GPUCA_BUILD_EVENT_DISPLAY_WAYLAND 1)
 set(GPUCA_BUILD_EVENT_DISPLAY_QT 1)
 set(GPUCA_CONFIG_GL3W 0)
 set(GPUCA_CONFIG_O2 1)
-set(GPUCA_BUILD_DEBUG 1)                    # Prevents float point exception at runtime (TODO: fix)
+set(GPUCA_BUILD_DEBUG 1)                    # Prevents float point exception at runtime and compiler warning (TODO: fix)
 set(GPUCA_BUILD_DEBUG_SANITIZE 0)
 set(GPUCA_BUILD_DEBUG_HOSTONLY 0)
 set(GPUCA_DETERMINISTIC_MODE GPU)           # OFF / NO_FAST_MATH / OPTO2 / GPU / WHOLEO2
@@ -38,7 +38,7 @@ set(CUDA_COMPUTETARGET "default" CACHE STRING "CUDA_COMPUTETARGET")  # 86 89
 #set(GPUCA_HIP_COMPILE_MODE perkernel)
 #set(GPUCA_RTC_NO_COMPILED_KERNELS 1)
 #set(GPUCA_KERNEL_RESOURCE_USAGE_VERBOSE 1)
-set(GPUCA_CONFIG_COMPILER /opt/gcc/bin/g++)             # gcc / clang / /opt/clang-p2996/bin/clang++
+set(GPUCA_CONFIG_COMPILER gcc)              # gcc / clang
 #set(GPUCA_CONFIG_WERROR 1)
 #add_definitions(-DGPUCA_GPU_DEBUG_PRINT)
 #set(GPUCA_OVERRIDE_PARAMETER_FILE "foo.csv")
@@ -46,10 +46,4 @@ set(GPUCA_CONFIG_COMPILER /opt/gcc/bin/g++)             # gcc / clang / /opt/cla
 if(GPUCA_CONFIG_COMPILER MATCHES "clang")
   add_compile_options(--gcc-toolchain=/cvmfs/alice.cern.ch/el9-x86_64/Packages/GCC-Toolchain/v14.2.0-alice2-1 -freflection-latest -std=c++26) # -stdlib=libc++
   add_link_options(--gcc-toolchain=/cvmfs/alice.cern.ch/el9-x86_64/Packages/GCC-Toolchain/v14.2.0-alice2-1 -lstdc++ -lm)
-else()
-  get_filename_component(_gcc_bin_dir "${GPUCA_CONFIG_COMPILER}" DIRECTORY)
-  get_filename_component(_gcc_root "${_gcc_bin_dir}" DIRECTORY)
-  list(APPEND CMAKE_INSTALL_RPATH "${_gcc_root}/lib64")
-  add_compile_options(-freflection -std=c++26)
 endif()
-
